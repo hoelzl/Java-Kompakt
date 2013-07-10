61f6fbb3f02c89b7a8467592ad38862a6cd9e8f6
raed <raed@pst.ifi.lmu.de>
2013-07-10 16:06:58 +0200

    Aufgabe 7.5 Wochentag Enumeration und Woche Klasse

---
+++ src/info/java_kompakt/kapitel_7/aufgabe_7_5/Wochentag.java
@@ -0,0 +1,13 @@
+package info.java_kompakt.kapitel_7.aufgabe_7_5;
+
+/**
+ * Die Enumeration <code>Wochentag</code> repräsentiert eine Aufzählungstyp.
+ * Ein Kurs hat einen Namen vom Typ String und ein Id von Typ int
+ * @author Allaithy Raed (raed@pst.ifi.lmu.de)
+ */
+
+public enum Wochentag {
+
+    MONTAG, DIENSTAG, MITTWOCH, DONNERSTAG, FREITAG, SAMSTAG, SONNTAG;
+
+}
